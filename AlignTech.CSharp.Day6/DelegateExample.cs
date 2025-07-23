9f1d3956dec69b211a4a7e5378f79d9bc0e65a1a
Arunkumar <arunkumar.e@outlook.com>
2025-07-23 10:32:01 +0530

    Day 6 - Delegate Example

---
+++ AlignTech.CSharp.Day6/DelegateExample.cs
@@ -0,0 +1,37 @@
+﻿namespace AlignTech.CSharp.Day6
+{
+    //Create a Delegate - Step 1
+    public delegate void MyDelegate(string msg);
+
+    public class DelegateExample
+    {
+        public void Message(string name)
+        {
+            Console.WriteLine($"Welcome, {name}");
+        }
+    }
+
+    //Multicast Delegate
+    public class LogError
+    {
+        public void LogToFile(string msg)
+        {
+            Console.WriteLine($"Error logged in Files : {msg}");
+        }
+
+        public void LogToConsole(string msg)
+        {
+            Console.WriteLine($"Error logged in Console :{msg}");
+        }
+
+        public static void StaticMethod(string  msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public static void StaticMethod1(string msg, string ex1)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
---
+++ AlignTech.CSharp.Day6/Program.cs
@@ -0,0 +1,21 @@
+﻿using AlignTech.CSharp.Day6;
+
+//DelegateExample obj = new DelegateExample();
+
+////Step 2: Assign or Reference a method
+//MyDelegate del = obj.Message;
+
+////Step 3: Call or Use Delegate
+//del("Pradeep");
+
+//MultiCast Delegate
+var log = new LogError();
+
+MyDelegate logError = log.LogToFile;
+logError += log.LogToConsole;
+
+logError += LogError.StaticMethod;
+
+//logError += LogError.StaticMethod1;
+
+logError("New Exception arised");
