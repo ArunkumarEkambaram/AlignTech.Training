0d64c5f454cb297af9ba8e7d383378a48893b0e6
Arunkumar <arunkumar.e@outlook.com>
2025-07-24 10:56:42 +0530

    Added Stack and HashTable

--- AlignTech.CSharp.Day7/NonGenericCollection.cs
+++ AlignTech.CSharp.Day7/NonGenericCollection.cs
@@ -36,18 +36,49 @@ namespace AlignTech.CSharp.Day7
             arrList.Add(new Product { ProductId = 2, ProductName = "Ball" });
             arrList.Add(new Product { ProductId = 3, ProductName = "Tennis Ball" });
             arrList.Add(100);
-            arrList.Add(300);
+            arrList.Add(300);            
+
+            if (arrList.Contains(100))
+            {
+                arrList.Remove(100);
+            }
 
             foreach (object item in arrList)
             {
-                if (item is Product)
+                if (item is Product prd)
                 {
-                    Product prd = (Product)item;
+                    // Product prd = (Product)item;
                     Console.WriteLine($"Product Id :{prd.ProductId}, ProductName :{prd.ProductName}");
                 }
                 else if (item is int)
                 {
                     Console.WriteLine(item);
+                }
+            }
+        }
+
+        //Hashtable
+        public void HashTableDemo()
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add(1, new Employee { EmpId = 1, EmpName = "Jagan" });
+            ht.Add(2, 1000);
+            ht.Add("1", 5000);
+
+            if (ht.ContainsKey(2))
+            {
+                ht.Remove(2);
+            }
+
+            foreach (DictionaryEntry item in ht)
+            {
+                if (item.Value is Employee emp)
+                {
+                    Console.WriteLine($"Emp ID :{emp.EmpId}, EmpName :{emp.EmpName}");
+                }
+                else
+                {
+                    Console.WriteLine(item.Value);
                 }
             }
         }
--- AlignTech.CSharp.Day7/Program.cs
+++ AlignTech.CSharp.Day7/Program.cs
@@ -1,8 +1,29 @@
-﻿
+﻿using AlignTech.CSharp.Day7;
+using System.Collections;
 
-using AlignTech.CSharp.Day7;
-
-NonGenericCollection obj=new NonGenericCollection();
+NonGenericCollection obj = new NonGenericCollection();
 //obj.TestMethod();
 
-obj.AddDetails();
+//obj.AddDetails();
+
+//obj.HashTableDemo();
+
+//Stack -- LIFO
+Stack stack = new Stack();
+stack.Push(100);
+stack.Push(600);
+stack.Push(5);
+stack.Push(-100);
+
+Console.WriteLine($"Last Value :{stack.Pop()}");
+
+foreach (var item in stack)
+{
+    Console.WriteLine(item);
+}
+
+//Collection Initializer
+ArrayList list = new ArrayList()
+{
+    100, true, 5000
+};
