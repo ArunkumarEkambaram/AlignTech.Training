7446fc3b4a18f1bc483ce02a1b972f6fbd3499c5
Arunkumar <arunkumar.e@outlook.com>
2025-07-29 11:32:17 +0530

    LINQ -

--- AlignTech.LINQ.Day10/Program.cs
+++ AlignTech.LINQ.Day10/Program.cs
@@ -6,8 +6,24 @@ var categoryData = new Category().GetCategories();
 //categoryData.Add(new Category { Id = 4 });
 
 //Query Expresssion
-var product = from p in productData 
-                        select p;
+var productQuery = from p in productData
+               where p.Price > 1000 && p.Name.Contains("p", StringComparison.OrdinalIgnoreCase)
+               select p;
 
+Console.WriteLine("Query Expresssion");
+foreach (var prd in productQuery)
+{
+    Console.WriteLine($"Product Id :{prd.Id}\tProduct Name :{prd.Name}\tPrice :{prd.Price}");
+}
+Console.WriteLine();
 
 //Lambda 
+var productLambda = productData
+                                     .Where(p => p.Price > 1000)
+                                     .Where(x => x.Name.Contains("p", StringComparison.OrdinalIgnoreCase));
+
+Console.WriteLine("Lambda Expresssion");
+foreach (var prd in productLambda)
+{
+    Console.WriteLine($"Product Id :{prd.Id}\tProduct Name :{prd.Name}\tPrice :{prd.Price}");
+}
